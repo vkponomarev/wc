33d0a50d122cc90581db5005db75cd4798c0a849
vkponomarev <vkponomarev@gmail.com>
2019-12-15 18:47:31 +0500

    Доработка возможности включения заголовка для Встроенных калькуляторов

--- frontend/views/partials/embed/_embed-hidden-input.php
+++ frontend/views/partials/embed/_embed-hidden-input.php
@@ -9,3 +9,6 @@
 <?php if ($this->params['isEmbed']): ?>
     <input type="hidden" name="embed" value="<?=$this->params['isEmbed']?>">
 <?php endif; ?>
+<?php if ($this->params['embedTitle']): ?>
+    <input type="hidden" name="title" value="<?=$this->params['embedTitle']?>">
+<?php endif; ?>
